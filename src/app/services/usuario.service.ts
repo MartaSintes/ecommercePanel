e37f9ef639646541190ff83d3ac12ae1ab4f1ae4
MartaSintes <78809511+MartaSintes@users.noreply.github.com>
2024-05-18 22:10:23 +0200

    Feat(edit)

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -17,20 +17,53 @@ export class UsuarioService {
       Authorization: token,
     });
     return this._http.post(this.url + '/usuario/create', data, {
-      headers: headers
+      headers: headers,
     });
   }
   getUsuarios(token: any): Observable<any> {
-    const headers = new HttpHeaders({ Authorization: token });
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
     return this._http.get(this.url + '/usuario', {
-      headers: headers
+      headers: headers,
+    });
+  }
+
+  getUsuario(id: any, token: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.get(this.url + '/usuario/getUsuario/' + id, {
+      headers: headers,
+    });
+  }
+
+  updateUsuario(id: any, data: any, token: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.put(this.url + '/usuario/updateUsuario/' + id, data, {
+      headers: headers,
+    });
+  }
+
+  deleteUsuario(id: any, token: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: token,
+    });
+    return this._http.delete(this.url + '/usuario/deleteUsuario/' + id, {
+      headers: headers,
     });
   }
 
   login(data: any): Observable<any> {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.post(this.url + '/usuario/login', data, {
-      headers: headers
+      headers: headers,
     });
   }
 }
